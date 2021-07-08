c49f0104ab41e57b244ea6c2eacfbcf3a5dd3c2a
subhanzaheer2003 <subhanzaheer2003@gmail.com>
2021-07-08 15:03:30 +0500

    Full Code Implemented

--- Header.h
+++ Header.h
@@ -1,1 +1,12 @@
 #pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+string rtrim(string& s, char c)
+{
+	string::iterator p;
+	for (p = s.end(); p != s.begin() && *--p == c;);
+	if (*p != c)p++;
+	s.erase(p, s.end());
+	return s;
+}
